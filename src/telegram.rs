a10f5264260276c1dca8bcba2bb565ea45d24ab7
Listum <listum@orudo.ru>
2023-11-02 05:37:45 +0300

    /command now downloads git repo

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,5 +1,7 @@
 use teloxide::{prelude::*, utils::command::BotCommands};
 use aur_rpc;
+use git2::Repository;
+
 
 pub async fn main(bot: Bot){
     Command::repl(bot, answer).await;
@@ -14,21 +16,26 @@ enum Command{
 }
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
-        Command::Upload(url) => {
-            bot.send_message(msg.chat.id, format!("Test {}", url)).await?;
+        Command::Upload(pkg) => {
+            let repo = format!("https://aur.archlinux.org/{}.git", pkg);
+            let dir = format!("pkgs/{}", pkg);
+            match Repository::clone(repo.as_str(), dir.as_str()) {
+                Ok(_) => bot.send_message(msg.chat.id, format!("Succsess {}", pkg)).await?,
+                Err(e) => bot.send_message(msg.chat.id, format!("Error: {}", e)).await?,
+            };
+
         }
         Command::Clean => {
             bot.send_message(msg.chat.id, format!("Done")).await?;
         }
         Command::Search(name) => {
-                    let packages = aur_rpc::search(format!("{}", name)).await.unwrap();
-                    let mut sorted_packages = packages;
-                    sorted_packages.sort_by(|a, b| b.num_votes.cmp(&a.num_votes));
-                    let mut result:Vec<String> = Vec::new();
-                    for (index, package) in sorted_packages.iter().enumerate().take(10) {
-                        result.push(format!("{}. {}\n", index+1, package.name));
+                    let mut packages = aur_rpc::search(format!("{}", name)).await.unwrap();
+                    packages.sort_by(|a, b| b.num_votes.cmp(&a.num_votes));
+                    let mut result = Vec::new();
+                    for (index, package) in packages.iter().enumerate().take(10) {
+                        result.push(format!("{}. {}", index+1, package.name));
                     }
-            bot.send_message(msg.chat.id, format!("{}", result.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(""))).await?;
+            bot.send_message(msg.chat.id, format!("{}", result.iter().map(|x| x.to_string()).collect::<Vec<String>>().join("\n"))).await?;
         }
     }
     Ok(())
